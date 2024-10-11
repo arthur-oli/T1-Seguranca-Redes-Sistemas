e62bc987f33e9e003c1373a6b254d0a34985352f
Arthur Oliveira <101880459+arthur-oli@users.noreply.github.com>
2024-10-10 23:23:48 -0300

    adicionado deciframento

--- caesar.py
+++ caesar.py
@@ -14,13 +14,38 @@ def remove_acento(char):
     else:
         return char
 
+def caesar_decipher(content, k):
+    result = []
+    
+    for char in content:
+        char = remove_acento(char)
+        new_char = chr(ord(char) - k)
+
+        if new_char > 'Z' and new_char < 'a':
+            upper = 'Z'
+            lower = 'a'
+
+        elif new_char > '9' and new_char < 'A':
+            upper = '9'
+            lower = 'A'
+
+        elif new_char < '0':
+            upper = 'z'
+            lower = '0'
+
+        else:
+            upper = ' '
+            lower = '!'
+        result.append(chr(ord(upper) - (ord(lower) - ord(new_char) - 1)))
+
+    return ''.join(result)
+
 def caesar_cipher(content, k):
     result = []
     
     for char in content:
         char = remove_acento(char)
         new_char = chr(ord(char) + k)
-        upper = lower = ' '
 
         if new_char > 'Z' and new_char < 'a':
             upper = 'a'
@@ -36,15 +61,22 @@ def caesar_cipher(content, k):
 
         else:
             upper = '!'
+            lower = ' '
         result.append(chr(ord(upper) + (ord(new_char) - ord(lower) - 1)))
 
     return ''.join(result)
 
 file_to_cypher = input("Qual o nome do arquivo aberto?")
 cypher_value = int(input("Quanto será o deslocamento da cifra?"))
+type = input("Cifrar ou decifrar?")
 
 with open(file_to_cypher + ".txt", "r") as file:
     file_content = file.read()
 
-result = caesar_cipher(file_content, cypher_value)
+if type == "cifrar":
+    result = caesar_cipher(file_content, cypher_value)
+
+elif type == "decifrar":
+    result = caesar_decipher(file_content, cypher_value)
+
 print(result)
